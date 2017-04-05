04174fec99a0b6f8b5cd3332442ccd7b9a1adcff
Meaghan Johnson <mejohn10@ncsu.edu>
2017-04-05 15:18:40 -0400

    elasticsearch functionality complete

--- mortar/dictionary_utils.py
+++ mortar/dictionary_utils.py
@@ -1,14 +1,30 @@
 from .models import ProjectTree, Category, AIDictionary, AIDictionaryObject
 from django.conf import settings
+from django.db import transaction
 import urllib.request
 import simplejson, json
 
+
+from .tree_utils import get_regex_list
+ 
 def update_dictionaries():
     dict_path = settings.DICTIONARIES_PATH
     
 
 def associate_tree(tree):
-    pass
-
+    terms = tree.categories.all()
+    for node in terms:
+        aidict = AIDictionary.objects.filter(name=node.name)
+        if len(aidict) > 0:
+            for word in aidict.words.all():
+                with transaction.atomic():
+                    new_node = Category.objects.get_or_create(
+                        name=word,
+                        parent=node
+                        projecttree=tree
+                        is_rule=False
+                    )
+                    new_node.save()
+            
 def build_analyzer_conf(tree):
     pass
--- mortar/elastic_utils.py
+++ mortar/elastic_utils.py
@@ -8,30 +8,61 @@ from elasticsearch import helpers
 
 from .tree_utils import get_regex_list
 
-def create_index(name, analysis_conf):
+def create_index(name, analysis_conf=None):
     es = settings.ES_CLIENT
     i_client = IndicesClient(client=es)
-    body = analysis_conf
+
+    if i_client.exists(name):
+        i_client.delete(name)
+
+    if analysis_conf:
+        body = analysis_conf
+    else:
+        body = create_analyzer_conf()
     i_client.create(index=name, body=body)
 
 def reindex(source, dest, query, update=True):
     es = settings.ES_CLIENT
     if update:
         # makes version consistent between indexes, updates doc if id already exists
-        helpers.reindex(client=es, source_index=source, target_index=dest, query=query, version_type=external)    
+        helpers.reindex(client=es, source_index=source, target_index=dest, query=query)    
     else:
         # version_type=internal dumps all documents, overwriting same ids. 
-        helpers.reindex(client=es, source_index=source, target_index=dest, query=query, version_type=internal)    
+        helpers.reindex(client=es, source_index=source, target_index=dest, query=query)    
 
-def build_mortar_query(tree):
-    # set base query
-    query = {
-        'query': {'match_all': {}},
-        'filter': {
-            'bool': {}
-        }
-    }   
+def build_mortar_query(terms):
     # get tree terms  
-    terms = get_regex_list(tree)
-    
-    return query
+    #terms = get_regex_list(tree)
+    to_query = []
+    for term in terms:
+       to_query.append(
+           { "match": {
+               "content": term  
+             }
+           }
+       )
+    query = { 
+        'query': {
+            'bool': {
+                'must': to_query
+            }   
+        }   
+    }
+    return query 
+
+def create_analyzer_conf():
+    # hardcode some analyzers for now
+    return {
+        "settings" : {
+          "analysis": {
+            "analyzer": {
+              "content" : {
+                "type": "pattern",
+                "pattern": "\\s+",
+                "filter": ["lowercase"]
+                
+              }
+            }
+          }
+        }
+    }
