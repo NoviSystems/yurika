af24549a8338323b44e55b90d81094a1a245a012
Ryan P Kilby <rpkilby@ncsu.edu>
2018-02-13 15:15:44 -0500

    Remove Google Analytics context

--- project/common_settings.py
+++ project/common_settings.py
@@ -112,7 +112,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'project.utils.context_processors.google_analytics',
                 'project.utils.context_processors.analysis_status',
                 'project.utils.context_processors.nav_current_page',
             ],
