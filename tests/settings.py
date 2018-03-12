f6d1086e7fb563eb78fe760c1f57871e9c0c9cec
Ryan P Kilby <rpkilby@ncsu.edu>
2018-03-12 07:51:09 -0400

    Add test settings using redislite

---
+++ tests/settings.py
@@ -0,0 +1,15 @@
+from redislite import Redis
+
+from project.common_settings import *  # flake8: noqa
+
+
+INSTALLED_APPS += [
+    'tests.pestle.testapp',
+]
+
+
+# Create a Redis instance using redislite
+redis = Redis(path('db.redis'))
+
+# Use redislite for the Celery broker
+CELERY_BROKER_URL = 'redis+socket://%s' % (redis.socket_file, )
