54e59e09476412fdeb6e02626cadca8dfcdae0f2
Ryan P Kilby <rpkilby@ncsu.edu>
2018-04-04 10:11:56 -0400

    Add Crawler model stub

--- mortar/models.py
+++ mortar/models.py
@@ -6,6 +6,11 @@ from django.db import models
 from django.utils import timezone
 from django_fsm import FSMField, transition
 from model_utils import Choices, managers
+from shortuuid import ShortUUID
+
+
+# Elasticsearch-friendly identifiers (no uppercase characters)
+b36 = ShortUUID(alphabet='0123456789abcdefghijklmnopqrstuvwxyz')
 
 
 def import_path(path):
@@ -18,10 +23,10 @@ def import_path(path):
 @signals.task_prerun.connect
 def start(sender, task_id, task, args, kwargs, **kw):
     try:
-        # Note that `kwargs['task_id']` is the Task's primary key value, and
+        # Note that `kwargs['task_pk']` is the Task's primary key value, and
         # the `task_id` argument is the Celery task ID, which is saved under
         # the same name on the task model.
-        task = Task.objects.select_subclasses().get(pk=kwargs.get('task_id'))
+        task = Task.downcast.get(pk=kwargs.get('task_pk'))
     except Task.DoesNotExist:
         return
 
@@ -36,7 +41,7 @@ def finish(sender, task_id, task, args, kwargs, retval, state, **kw):
         return
 
     try:
-        task = Task.objects.select_subclasses().get(pk=kwargs.get('task_id'))
+        task = Task.downcast.get(task_id=task_id)
     except Task.DoesNotExist:
         return
 
@@ -47,7 +52,7 @@ def finish(sender, task_id, task, args, kwargs, retval, state, **kw):
 @signals.task_revoked.connect
 def abort(sender, request, terminated, **kw):
     try:
-        task = Task.objects.select_subclasses().get(pk=request.task_id)
+        task = Task.downcast.get(task_id=request.task_id)
     except Task.DoesNotExist:
         return
 
@@ -58,12 +63,21 @@ def abort(sender, request, terminated, **kw):
 @signals.task_failure.connect
 def error(sender, task_id, exception, args, kwargs, traceback, **kw):
     try:
-        task = Task.objects.select_subclasses().get(pk=kwargs.get('task_id'))
+        task = Task.downcast.get(task_id=task_id)
     except Task.DoesNotExist:
         return
 
     task._error()
     task.save()
+
+
+class CastingManager(managers.InheritanceManager):
+    """
+    Manager that automatically downcasts instances to their inherited types.
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().select_subclasses()
 
 
 class Task(models.Model):
@@ -103,6 +117,10 @@ class Task(models.Model):
     finished_at = models.DateTimeField(null=True, editable=False)
 
     objects = managers.InheritanceManager()
+    downcast = CastingManager()
+
+    class Meta:
+        base_manager_name = 'downcast'
 
     @property
     def task_path(self):
@@ -113,7 +131,7 @@ class Task(models.Model):
         task = import_path(self.task_path)
 
         # immutable arg prevents results from being passed in task chain
-        return task.signature([], {'task_id': self.pk}, immutable=True)
+        return task.signature([], {'task_pk': self.pk}, immutable=True)
 
     def abort(self, *, signal='SIGTERM'):
         revoke(self.task_id, terminate=True, signal=signal)
@@ -158,3 +176,37 @@ class TaskError(models.Model):
         if self.type:
             return "Task Error {}: {}".format(self.type, self.message)
         return "Task Error: {}".format(self.message)
+
+
+class Crawler(models.Model):
+    index = models.UUIDField(unique=True, editable=False, default=b36.uuid,
+                             help_text="Elasticsearch index name for crawled documents.")
+
+    def __str__(self):
+        return 'Crawler: %s' % self.pk
+
+
+class URLSeed(models.Model):
+    crawler = models.ForeignKey(Crawler, on_delete=models.CASCADE, related_name='seeds')
+    url = models.URLField()
+
+
+class CrawlerTask(Task):
+    crawler = models.OneToOneField(Crawler, on_delete=models.CASCADE, related_name='task')
+
+    @property
+    def task_path(self):
+        return 'mortar.tasks.crawl'
+
+    def abort(self, *, clean=True):
+        """
+        Either abort the crawler cleanly, or not. A clean abort will allow
+        subsequent celery tasks to complete, while an unclean abort will kill
+        the remainder of the task chain.
+
+        A clean abort is useful when you want to end crawling, but then start
+        subsequent tasks (like preprocessing). The caveat is that a clean abort
+        takes a few seconds for Scrapy to shutdown the crawler and exit, while
+        an unclean abort is immediate.
+        """
+        super().abort(signal='SIGTERM' if clean else 'SIGABRT')
