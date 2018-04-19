97f6e1a148481a8a10320e7f1b2c539d7de05307
Ryan P Kilby <rpkilby@ncsu.edu>
2018-04-19 12:04:05 -0400

    Add twisted exception logging

--- mortar/crawler/__init__.py
+++ mortar/crawler/__init__.py
@@ -10,10 +10,19 @@ def crawl(task):
     # prevent scrapy from mucking with our logging configuration
     log.dictConfig = lambda _: _
 
+    def twisted_exc(failure):
+        # it's necessary to manually log the exception, as twisted loses the
+        # real traceback object, but retains the frames to semi-rebuild it.
+        task.errors.create(
+            message=str(failure.value),
+            traceback=failure.getTraceback(),
+        )
+
     process = CrawlerProcess({
         # enables state persistence, allowing crawler to be paused/unpaused
         'JOBDIR': task.crawler.state_dir
     }, install_root_handler=False)
 
-    process.crawl(WebCrawler, task=task)
+    d = process.crawl(WebCrawler, task=task)
+    d.addErrback(twisted_exc)
     process.start()
