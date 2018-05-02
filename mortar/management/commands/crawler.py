e7eb7bbd930ec3e1f925897db87aca4bee3f391b
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-02 16:49:48 -0400

    Factor out command utility code

--- mortar/management/commands/crawler.py
+++ mortar/management/commands/crawler.py
@@ -15,6 +15,7 @@ from terminaltables.other_tables import WindowsTable as SingleTable
 
 from mortar import models
 from project import utils
+from .utils import side_by_side, style_by_line, truncate_message
 
 RESTART_HELP = """
 Clear a crawler's persistent state and restart it.
@@ -99,46 +100,6 @@ def domains(filename):
             raise ArgumentTypeError(f"{pre} - domain must not contain a path")
 
     return domains
-
-
-def side_by_side(left, right, spacer=8):
-    """
-    Format two blocks of text to display side-by-side.
-
-    It is assumed that each block of text is consistent in width.
-    """
-    left = left.split('\n')
-    right = right.split('\n')
-
-    # total number of lines
-    lines = max(len(left), len(right))
-    spacers = [' ' * spacer] * lines
-
-    # add blank lines to left side
-    blank = ' ' * len(left[0])
-    left.extend([blank] * (lines - len(left)))
-
-    # add blank lines to right side
-    blank = ' ' * len(right[0])
-    right.extend([blank] * (lines - len(right)))
-
-    return '\n'.join([''.join(parts) for parts in zip(left, spacers, right)])
-
-
-def style_by_line(text, style_func):
-    return '\n'.join([style_func(line) for line in text.splitlines()])
-
-
-def truncate_message(message, max_width):
-    # truncate extra lines to single line + ellipsis
-    lines = message.splitlines()
-    message = lines[0] + '...' if len(lines) > 1 else lines[0]
-
-    # truncate to width + ellipsis
-    if len(message) > max_width:
-        message = message[:max_width - 3] + '...'
-
-    return message
 
 
 class Command(BaseCommand):
