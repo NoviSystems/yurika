d47d81fe06ae96862754046a8a02f101e59d977e
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-14 15:23:32 -0400

    Update crawler integration tests
    By adding a slow view, we can use the timing to ensure that we stop
    the crawler midway.

--- tests/integration/mortar/test_crawlers.py
+++ tests/integration/mortar/test_crawlers.py
@@ -2,6 +2,7 @@ import time
 from urllib.parse import urljoin
 
 from django.test import LiveServerTestCase, override_settings
+from django.urls import reverse
 from django_dramatiq.test import DramatiqTestCase
 
 from mortar import models
@@ -13,23 +14,21 @@ STATUS = models.Task.STATUS
 @override_settings(ROOT_URLCONF='tests.integration.mortar.testapp.urls')
 class CrawlerTests(DramatiqTestCase, LiveServerTestCase):
 
-    def test_simple(self):
-        base_url = self.live_server_url
-
+    def test_crawl(self):
         # create a crawler and it's management task
-        crawler = models.Crawler.objects.create(start_urls=urljoin(base_url, 'a'))
-        task = crawler.task
+        url = urljoin(self.live_server_url, reverse('a'))
+        crawler = models.Crawler.objects.create(start_urls=url)
 
-        # send off the task
-        task.send()
+        # start the task
+        crawler.start()
 
         # wait for the task to complete
-        self.broker.join(task.task.queue_name)
+        self.broker.join(crawler.task.task.queue_name)
         self.worker.join()
 
         # the task should be done
-        task.refresh_from_db()
-        self.assertEqual(task.status, STATUS.done)
+        crawler.task.refresh_from_db()
+        self.assertEqual(crawler.task.status, STATUS.done)
 
         # ensure the index has been populated
         crawler.index.refresh()
@@ -37,56 +36,75 @@ class CrawlerTests(DramatiqTestCase, LiveServerTestCase):
         # and there should be three crawled documents
         self.assertEqual(crawler.documents.search().count(), 3)
 
-    def test_revoke(self):
-        base_url = self.live_server_url
-
+    def test_stop(self):
         # create a crawler and it's management task
-        crawler = models.Crawler.objects.create(start_urls=urljoin(base_url, 'a'))
-        task = crawler.task
+        url = urljoin(self.live_server_url, reverse('ref-slow'))
+        crawler = models.Crawler.objects.create(start_urls=url)
 
-        # send off the task and immediately revoke task
-        task.send()
-        task.revoke()
+        # send off the task and revoke task
+        crawler.start()
+        time.sleep(.5)  # 'slow' view takes .5 seconds
+        crawler.task.refresh_from_db()
+        crawler.stop()
 
         # wait for the task to complete
-        self.broker.join(task.task.queue_name)
+        self.broker.join(crawler.task.task.queue_name)
         self.worker.join()
 
         # the task should be aborted
-        task.refresh_from_db()
-        self.assertEqual(task.status, STATUS.aborted)
+        crawler.task.refresh_from_db()
+        self.assertEqual(crawler.task.status, STATUS.aborted)
 
         # ensure the index has been populated
         crawler.index.refresh()
 
         # and there should be less than three crawled documents
-        self.assertLess(crawler.documents.search().count(), 3)
+        self.assertGreater(crawler.documents.search().count(), 0)
+        self.assertLess(crawler.documents.search().count(), 4)
 
     def test_resume(self):
-        # Start and revoke a crawler
-        self.test_revoke()
+        # Start and stop a crawler
+        self.test_stop()
 
         # then resume the crawler
         crawler = models.Crawler.objects.get()
-        crawler.restart()
-        task = crawler.task
-
-        # send off the task
-        task.send()
+        crawler.resume()
 
         # wait for the task to complete
-        self.broker.join(task.task.queue_name)
+        self.broker.join(crawler.task.task.queue_name)
         self.worker.join()
 
         # the task should be done
-        task.refresh_from_db()
-        self.assertEqual(task.status, STATUS.done)
+        crawler.task.refresh_from_db()
+        self.assertEqual(crawler.task.status, STATUS.done)
 
         # ensure the index has been populated
         crawler.index.refresh()
 
-        # and there should be three crawled documents
-        self.assertEqual(crawler.documents.search().count(), 3)
+        # and there should be four crawled documents
+        self.assertEqual(crawler.documents.search().count(), 4)
+
+    def test_restart(self):
+        # Start and stop a crawler
+        self.test_stop()
+
+        # then restart the crawler
+        crawler = models.Crawler.objects.get()
+        crawler.restart()
+
+        # wait for the task to complete
+        self.broker.join(crawler.task.task.queue_name)
+        self.worker.join()
+
+        # the task should be done
+        crawler.task.refresh_from_db()
+        self.assertEqual(crawler.task.status, STATUS.done)
+
+        # ensure the index has been populated
+        crawler.index.refresh()
+
+        # and there should be *more* than four crawled documents
+        self.assertGreater(crawler.documents.search().count(), 4)
 
 
 class OpenCrawlerTests(DramatiqTestCase):
--- tests/integration/mortar/testapp/urls.py
+++ tests/integration/mortar/testapp/urls.py
@@ -1,3 +1,5 @@
+import time
+
 from django.conf.urls import include, url
 from django.http import HttpResponse
 from django.urls import reverse
@@ -43,10 +45,38 @@ def view_c(response):
     """)
 
 
+def view_slow(response):
+    url_a = reverse('a')
+
+    time.sleep(.5)
+
+    return HttpResponse(f"""
+        <html>
+        <body>
+            <a href="{url_a}">Link to a</a>
+        </body>
+        </html>
+    """)
+
+
+def view_ref_slow(response):
+    url_slow = reverse('slow')
+
+    return HttpResponse(f"""
+        <html>
+        <body>
+            <a href="{url_slow}">Link to slow</a>
+        </body>
+        </html>
+    """)
+
+
 urlpatterns = [
     url(r'^a$', view_a, name='a'),
     url(r'^b$', view_b, name='b'),
     url(r'^c$', view_c, name='c'),
+    url(r'^slow$', view_slow, name='slow'),
+    url(r'^ref_slow$', view_ref_slow, name='ref-slow'),
     url(r'^$', view_c, name='home'),
     url(r'^', include(authpatterns)),
 ]
