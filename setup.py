4c848706bfaac77738663a6fbd39683f31bf8173
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-24 01:30:22 -0400

    Add setup module and main entry point

---
+++ setup.py
@@ -0,0 +1,72 @@
+import io
+
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with io.open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+
+requirements = [
+    # Project settings requirements
+    'Django~=1.11',
+    'django-environ>=0.4.3',
+    'colorlog',
+
+    # Django applications
+    'djangorestframework',
+    'django-dramatiq',
+    'django-fsm',
+    'django-model-utils',
+
+    # Additional dependencies
+    'beautifulsoup4',
+    'dramatiq>=1.1.0',
+    'elasticsearch>=6',
+    'elasticsearch-dsl>=6',
+    'jsonfield2',
+    'nltk',
+    'progressbar2',
+    'redis',
+    'scrapy',
+    'shortuuid',
+    'terminaltables',
+]
+
+
+setup(
+    name='yurika',
+    version='0.0.0',
+    author='ITNG',
+    url='https://github.com/ITNG/yurika',
+    description='Unstructured Text Analytics Platform',
+    long_description=get_long_description(),
+    license='BSD',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        'sentry': ['raven'],
+        'dev': ['tox', 'tox-venv', 'pip-tools'],
+    },
+    entry_points={
+        'console_scripts': [
+            'yurika = yurika.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Scrapy',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
--- src/yurika/__main__.py
+++ src/yurika/__main__.py
@@ -2,12 +2,10 @@
 import os
 import sys
 
-import environ
 
-if __name__ == "__main__":
-    envfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
-    if os.path.exists(envfile):
-        environ.Env.read_env(envfile)
+def main():
+    sys.path.insert(0, os.environ['YURIKA_CONF'])
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     try:
         from django.core.management import execute_from_command_line
@@ -18,3 +16,7 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
