3f7bc4829967888960ebcd25e6a2ef250bf8468d
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-31 17:15:31 -0400

    Improve crawl task error handling

--- src/yurika/mortar/tasks.py
+++ src/yurika/mortar/tasks.py
@@ -8,8 +8,8 @@ from . import crawler, models
 
 @dramatiq.actor(max_retries=0, notify_shutdown=True)
 def crawl(task_id):
-    # NOTE: wrapping the crawler in a task enables pipelining,
-    #       otherwise this would be unnecessary indirection.
+    # NOTE: wrapping the crawler in a task enables pipelining and offloading to
+    #       a remote worker. Otherwise this would be unnecessary indirection.
     task = models.CrawlerTask.objects.get(pk=task_id)
 
     proc = Process(target=crawler.crawl, args=(task, ))
@@ -27,7 +27,16 @@ def crawl(task_id):
             proc.join(.5)
 
     except (Shutdown, TimeLimitExceeded):
+        # Shutdown/TimeLimitExceeded are interrupts, not errors.
+        raise
+
+    except Exception as exc:
+        task.log_exception(exc)
+        raise
+
+    finally:
         proc.terminate()
         proc.join()
 
-        raise
+        if proc.exitcode != 0:
+            task.log_error(f'Crawler returned a non-zero exit code: {proc.exitcode}')
